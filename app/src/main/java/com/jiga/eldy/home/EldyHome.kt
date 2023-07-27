8dbecbe47f1134df69c02657004d6f5f9a45eaf5
Eldy <eldyapp@gmail.com>
2023-07-27 12:05:33 -0500

    Initial commit

---
+++ app/src/main/java/com/jiga/eldy/home/EldyHome.kt
@@ -0,0 +1,12 @@
+package com.jiga.eldy.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.jiga.eldy.R
+
+class EldyHome : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_eldy_home)
+    }
+}
